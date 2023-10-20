6b2beea7f87bf385755d7307df1a394bb6f4dbe2
rogeriosantos232 <131918266+rogeriosantos232@users.noreply.github.com>
2023-10-19 22:23:52 -0300

    att exercicio

---
+++ Exercicio/exercicio.js
@@ -0,0 +1,26 @@
+const caixa1 =document.querySelector("#caixa1")
+const caixa2 =document.querySelector("#caixa2")
+const btn =document.querySelector("#btn_copiar")
+const todosCursos =[...document.querySelectorAll(".curso")]
+
+todosCursos.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        curso = evt.target
+        curso.classList.toggle("selecionado")
+    })
+})
+
+btn.addEventListener('click',()=>{
+    const cursosSelecionados = [...document.querySelectorAll(".selecionado")]
+
+    const cursosNaoSelecionados = [...document.querySelectorAll(".curso:not(.selecionado)")]
+
+    cursosSelecionados.map((el)=>{
+        caixa2.appendChild(el)
+    })
+
+    cursosNaoSelecionados.map((el)=>{
+        caixa1.appendChild(el)
+    })
+
+})
